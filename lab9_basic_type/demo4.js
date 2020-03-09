a886a98b30b238acec418ce78cf2be492fc9e569
Brad Chen <ctchen01@hotmail.com>
2020-03-09 17:33:15 +0800

    lab8 to lab9

---
+++ lab9_basic_type/demo4.js
@@ -0,0 +1,8 @@
+console.log(512*512) // 乘法計算
+console.log(2147483647+9999999999) // 12147483646 超過int最大值也能計算
+console.log(0.4+0.2) // 0.6000000000000001 小數計算不一定精準
+console.log(0.11+0.12==0.23) // false
+console.log(1-0.4==0.6)  // true
+console.log(1-0.5==0.5)  // true
+console.log(1-0.75==0.25)  // true
+console.log(1-0.3+0.1==0.8)  // false
